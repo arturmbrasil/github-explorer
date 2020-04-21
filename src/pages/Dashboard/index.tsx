ffd4d3e72cd6bcb34ab45eed4bc534504d0c1b54
Artur Brasil <arturmbrasil@hotmail.com>
2020-04-21 16:27:12 -0300

    dashboard styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,76 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/22780735?s=460&u=e6842bd327e085392747fab72fc49bbaed38d134&v=4"
+            alt="Artur Brasil"
+          />
+          <div>
+            <strong>arturmbrasil/be-the-hero</strong>
+            <p>Esta é uma descricao teste...</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/22780735?s=460&u=e6842bd327e085392747fab72fc49bbaed38d134&v=4"
+            alt="Artur Brasil"
+          />
+          <div>
+            <strong>arturmbrasil/be-the-hero</strong>
+            <p>Esta é uma descricao teste...</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/22780735?s=460&u=e6842bd327e085392747fab72fc49bbaed38d134&v=4"
+            alt="Artur Brasil"
+          />
+          <div>
+            <strong>arturmbrasil/be-the-hero</strong>
+            <p>Esta é uma descricao teste...</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/22780735?s=460&u=e6842bd327e085392747fab72fc49bbaed38d134&v=4"
+            alt="Artur Brasil"
+          />
+          <div>
+            <strong>arturmbrasil/be-the-hero</strong>
+            <p>Esta é uma descricao teste...</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
